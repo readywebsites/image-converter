412daea6b591c3acd853829b517676aad0d0fd73
fatemadhalech16 <fatemadhalech16@gmail.com>
2026-07-09 18:11:17 +0530

    allowed host

--- converter_project/settings.py
+++ converter_project/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://cloudconvert.biz499.com/"
 ]
 
 ROOT_URLCONF = 'converter_project.urls'
